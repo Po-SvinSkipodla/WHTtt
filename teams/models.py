41392c31467523fb8c92d457e93bda9c505b53de
Vlad <vladislav.zhyla@gmail.com>
2023-10-23 18:42:50 +0300

    Added models

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models import SET_NULL
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254, unique=True)
+    team = models.ForeignKey(to=Team, null=True, on_delete=SET_NULL)
+
+    class Meta:
+        unique_together = ("first_name", "last_name",)
