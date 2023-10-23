0de59391dafb671e4a2e671159ba580b727613f3
Vlad <vladislav.zhyla@gmail.com>
2023-10-23 18:43:03 +0300

    Added serializers

---
+++ teams/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from teams.models import Team, Member
+
+
+class TeamSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Team
+        fields = "__all__"
+
+
+class MemberSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Member
+        fields = ("__all__")
+
+
+class MemberListSerializer(MemberSerializer):
+    team = serializers.StringRelatedField()
