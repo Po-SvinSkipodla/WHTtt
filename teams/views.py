de5f309fab40ed44fe81fcbaad58db34d0f2ccb7
Vlad <vladislav.zhyla@gmail.com>
2023-10-23 18:43:19 +0300

    Added views

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from teams.models import Team, Member
+from teams.serializers import TeamSerializer, MemberSerializer, MemberListSerializer
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list" or self.action == "retrieve":
+            return MemberListSerializer
+
+        return MemberSerializer
